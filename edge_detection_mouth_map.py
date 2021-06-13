90eb90d1582a6ec997b2be425e35e21b011e7ee6
Daniel98p <danielmichalik98@wp.pl>
2021-06-13 21:19:44 +0200

    Search mouth candidates

---
+++ edge_detection_mouth_map.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+from basic_functions import read_image
+from luminance_correction import luminance_correction
+from mouth_candidates import create_mouth_map_Fg
+import matplotlib.pyplot as plt
+from face_candidates import fit_ellipse, create_binary_mask
+
+if __name__ == '__main__':
+    img = read_image("data/38-1m.bmp")
+    img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    luminance_correction(img_rgb)
+    mask = create_binary_mask(img_rgb)
+    ellipse_img = fit_ellipse(mask, img_rgb)
+    imgYCrCb = cv.cvtColor(ellipse_img, cv.COLOR_RGB2YCrCb)
+
+    mouth_map = create_mouth_map_Fg(imgYCrCb)
+    mouth_map_rgb = cv.cvtColor(mouth_map, cv.COLOR_YCrCb2RGB)
+    mouth_map_gray = cv.cvtColor(mouth_map_rgb, cv.COLOR_RGB2GRAY)
+    ret, th = cv.threshold(mouth_map_gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    th = cv.bitwise_not(th)
+    # show_images([mouth_map, th], 2, 1)
+    plt.subplot(1, 2, 1)
+    plt.imshow(mouth_map, cmap="gray")
+    plt.subplot(1, 2, 2)
+    plt.imshow(th, cmap="gray")
+    plt.show()
